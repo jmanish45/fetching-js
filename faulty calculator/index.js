0fa006dc779a64cb291acd7eaa07bf3cc1c86012
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-11 13:46:16 +0530

    js mini project1

--- faulty calculator/index.js
+++ faulty calculator/index.js
@@ -1,20 +1,43 @@
-let random = Math.random();
+// let random = Math.random();
 
-let a = prompt("Enter first numser : ")
-let b = prompt("Enter operation to perform : ")
-let c =prompt("Enter second number :")
+// let a = prompt("Enter first numser : ")
+// let b = prompt("Enter operation to perform : ")
+// let c =prompt("Enter second number :")
 
-let obj = {
-    "+" : "-",
-    "-" : "+",
-    "*" : "/",
-    "/" : "*",
+// let obj = {
+//     "+" : "-",
+//     "-" : "+",
+//     "*" : "/",
+//     "/" : "*",
 
-}
- if(random  > 0.1) {
-    alert(`the result is ${eval(`${a} ${b} ${c}`)}`)
+// }
+//  if(random  > 0.1) {
+//     alert(`the result is ${eval(`${a} ${b} ${c}`)}`)
+//  }
+//  else {
+//     b = obj[b];
+//     alert(`the faulty result is ${eval(`${a} ${b} ${c}`)}`)
+//  }
+
+//  function getData(dataId) {
+//    console.log("data", dataId);
+//  }
+
+ function getdata(dataid, getnextdata) {
+   setTimeout(() => {
+      console.log("data ", dataid);
+      if(getnextdata) {
+         getnextdata();
+      }
+
+   }, 2000);
  }
- else {
-    b = obj[b];
-    alert(`the faulty result is ${eval(`${a} ${b} ${c}`)}`)
- }
+
+ getdata (1, () => {
+   getdata(2, () => {
+      getdata(3);
+   });
+ });
+
+
+
